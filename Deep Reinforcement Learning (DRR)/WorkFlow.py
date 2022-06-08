914d4886bf5789023a0c0eb1ce7ff000fd1ce9b6
Muhammad Ilham Malik <79507977+ilhamMalik51@users.noreply.github.com>
2022-06-08 11:47:46 +0700

    Upload updated workflow

--- Deep Reinforcement Learning (DRR)/WorkFlow.py
+++ Deep Reinforcement Learning (DRR)/WorkFlow.py
@@ -5,11 +5,19 @@ import tensorflow as tf
 from model import Actor, Critic, DRRAveStateRepresentation, PMF
 from utils.history_buffer import HistoryBuffer
 
+def load_parameter():
+    reward_function.load_weights('trained/pmf_weights/pmf_150')
+    actor_net.load_weights('trained/actor_weights/actor_150')
+    critic_net.load_weights('trained/critic_weights/critic_150')
+    state_rep_net.load_weights('trained/state_rep_weights/state_rep_150')
+
 ## ngebuka data
 users = pickle.load(open('dataset_RL/user_id_to_num.pkl', 'rb'))
 items = pickle.load(open('dataset_RL/item_lookup.pkl', 'rb'))
 data = np.load('dataset_RL/data_RL_25000.npy')
 data[:, 2] = 0.5 * (data[:, 2] - 3)
+
+
 
 ## deklarasi model
 state_rep_net = DRRAveStateRepresentation(5, 100, 100)
@@ -22,112 +30,125 @@ actor_net.build_model()
 critic_net.build_model()
 reward_function.build_model()
 ## load pretrained
-reward_function.load_weights('trained/pmf_weights/pmf_150')
-actor_net.load_weights('trained/actor_weights/actor_150')
-critic_net.load_weights('trained/critic_weights/critic_150')
-state_rep_net.load_weights('trained/state_rep_weights/state_rep_150')
+
+## load data
+train_data = tf.convert_to_tensor(data[:int(0.8 * data.shape[0])], dtype='float32')
 
 ## sifat user dan item
 ## untuk candidate item buat dihitung
 user_embeddings = tf.convert_to_tensor(reward_function.user_embedding.get_weights()[0])
-candidate_items = tf.convert_to_tensor(reward_function.item_embedding.get_weights()[0])
+item_embeddings = tf.convert_to_tensor(reward_function.item_embedding.get_weights()[0])
 
 ## Declare Deque dari utils
 history_buffer = HistoryBuffer(5)
-## user buat sementara diacak dalam pemilihan embeddingnya
-user_idx = np.random.randint(0, 549)
-## ambil embedding user/sifat user
-user_emb = user_embeddings[user_idx]
+candidate_items = tf.identity(tf.stop_gradient(item_embeddings))
 
-## pake data sampel buat ngambil
-## 5 item terakhir yang disukai user
-user_reviews = data[data[:, 0] == 3]
+## random user
+e = np.random.randint(0, len(users))
+print("User Indeks ke-{}".format(e))
+
+## user review ini bisa diacak" diganti aja angka 3-nya
+## bisa pake variabel 'e' atau angka sendiri
+user_reviews = train_data[train_data[:, 0] == 3]
+## kalo ini variabel ngambil data yang sebelumnya disuka
 pos_user_reviews = user_reviews[user_reviews[:, 2] > 0]
 
-## history buffer itu gunanya buat nyimpen
-## 5 item yang sebelumnya disukai user
-for i in range(5):
-    emb = candidate_items[int(pos_user_reviews[i, 1])]
-    history_buffer.push(tf.identity(tf.stop_gradient(emb)))
+if pos_user_reviews.shape[0] < 5:
+    while pos_user_reviews.shape[0] < 5:
+        e = np.random.randint(0, len(users))
+        user_reviews = train_data[train_data[:, 0] == e]
+        pos_user_reviews = user_reviews[user_reviews[:, 2] > 0]
 
-## list item yang tidak terpilih
-ignored_items = []
+## copas item embedding
+candidate_items = tf.identity(tf.stop_gradient(item_embeddings))
 
-## program mulai
-t = 0
-episode_length = 5
+int_user_reviews_idx = user_reviews[:, 1].numpy().astype(int)
+user_candidate_items = tf.identity(tf.stop_gradient(tf.gather(item_embeddings, indices=int_user_reviews_idx)))
+
+user_emb = user_embeddings[e]
 
 ## item terakhir yang dilike/ga dilike
 ## yang like positif yang ga like negatif
-item_yg_udh_dinilai = [[0, 0.8], [1, 0.8], [2, -0.8], [3, -0.8], [4, 0.8]]
+## bisa eksplor" saja
 
-## variabel buat nampung item hasil rekomendasi
-item_for_recommendation = []
+## ========== DUMMY DATA ============= ##
+# last_item = [[0, 0.8], [50, 0.8], [2, -0.8], [120, -0.8], [86, -0.8]]
+# last_item = [[20, 0.8], [1, 0.8], [2, -0.8], [3, 0.8], [86, -0.8]]
+# last_item = [[100, 0.8], [5, 0.8], [25, 0.8], [45, 0.8], [75, 0.8]]
+last_item = [[30, -0.8], [49, 0.8], [8, -0.8], [12, -0.8], [20, -0.8]]
+## ========== DUMMY DATA ============= ##
 
-for item in item_yg_udh_dinilai:
-
-    if item[1] > 0:
-        ## ambil embeddingnya terus push ke buffer history
-        emb = candidate_items[item[0]]
+ignored_items = []
+print("Terakhir Liked Items:")
+for i in range(5):
+    ## ini buat ngecek apakah lastnya itu dilike atau engga
+    ## kalo dilike masukin buffer buat diproses
+    if last_item[i][1] > 0:
+        emb = candidate_items[last_item[i][0]]
+        history_buffer.push(emb)
+        print(items.get(last_item[i][0]))
+        ignored_items.append(last_item[i][0])
+        
+if len(history_buffer) < 5:
+    i = 0
+    while len(history_buffer) < 5:
+        emb = candidate_items[int(pos_user_reviews[i, 1].numpy())]
         history_buffer.push(tf.identity(tf.stop_gradient(emb)))
+        i+=1
 
-        ## statenya hitung
-        state = state_rep_net(user_emb, tf.stack(history_buffer.to_list()))
+print("=====================")
+print("Panjang Item yang diabaikan: {}".format(len(ignored_items)))
 
-        ## actionnya hitung
-        action = actor_net(tf.stop_gradient(state))
+t = 0
+explore = 0.2
+state = None
+action = None
+reward = None
+next_state = None
+recommend_item = []
+user_ignored_items = []
 
-        # Perhitungan skor ranking item terbaik dan di-flatten
-        ranking_scores = candidate_items @ tf.transpose(action)
-        ranking_scores = tf.reshape(ranking_scores, (ranking_scores.shape[0],)).numpy()
-
-        ## ini jaga" biar ga direkomen dua kali
-        if len(ignored_items) > 0:
-            rec_items = tf.stack(ignored_items).numpy()
-        else:
-            rec_items = []
-
-        ## nanti di list rankingnya dinegatifin biar di akhir posisinya
-        ranking_scores[rec_items] = -float("inf")
-
-        # ambil rekomen itemnya
-        rec_item_idx = tf.argmax(ranking_scores).numpy()
-        rec_item_emb = candidate_items[rec_item_idx]
-
-        item_for_recommendation.append(rec_item_idx)
-
-        # Remove new item from future recommendations
-        ignored_items.append(tf.convert_to_tensor(rec_item_idx))
-
+load_parameter()
+while t < 10:
+    state = state_rep_net(user_emb, tf.stack(history_buffer.to_list()), training=False)
+    if np.random.uniform(0, 1) < explore:
+        action = tf.convert_to_tensor(0.1 * np.random.rand(1,100), dtype='float32')
     else:
-        ## statenya dihitung
-        ## karena itemnya gadilike jadi keadaan state masih sama
-        state = state_rep_net(user_emb, tf.stack(history_buffer.to_list()))
-
-        ## actionnya hitung
-        action = actor_net(tf.stop_gradient(state))
-
-        # Perhitungan skor ranking item terbaik dan di-flatten
-        ranking_scores = candidate_items @ tf.transpose(action)
-        ranking_scores = tf.reshape(ranking_scores, (ranking_scores.shape[0],)).numpy()
-
-        ## ini jaga" biar ga direkomen dua kali
-        if len(ignored_items) > 0:
-            rec_items = tf.stack(ignored_items).numpy()
-        else:
-            rec_items = []
-
-        ## nanti di list rankingnya dinegatifin biar di akhir posisinya
-        ranking_scores[rec_items] = -float("inf")
-
-        # ambil rekomen itemnya
-        rec_item_idx = tf.argmax(ranking_scores).numpy()
-        rec_item_emb = candidate_items[rec_item_idx]
-
-        item_for_recommendation.append(rec_item_idx)
-
-        # Remove new item from future recommendations
-        ignored_items.append(tf.convert_to_tensor(rec_item_idx))
-
-for indeks in item_for_recommendation:
-    print(items.get(indeks))
+        action = actor_net(state, training=False)
+        
+    ranking_scores = candidate_items @ tf.transpose(action)
+    ranking_scores = tf.reshape(ranking_scores, (ranking_scores.shape[0],)).numpy()
+    
+    if len(ignored_items) > 0:
+        rec_items = tf.stack(ignored_items).numpy().astype(int)
+        rec_items = np.array(rec_items)
+    else:
+        rec_items = []
+    
+    user_rec_items = tf.stack(user_ignored_items) if len(user_ignored_items) > 0 else []
+    user_rec_items = np.array(user_rec_items)
+    
+    ranking_scores[rec_items if len(ignored_items) > 0 else []] = -float("inf")
+    user_ranking_scores = tf.gather(ranking_scores, indices=user_reviews[:, 1].numpy().astype(int)).numpy()
+    user_ranking_scores[user_rec_items if len(user_ignored_items) > 0 else []] = -float("inf")
+    
+    rec_item_idx = tf.math.argmax(user_ranking_scores).numpy()    
+    rec_item_emb = user_candidate_items[rec_item_idx]
+    
+    recommend_item.append(rec_item_idx)
+    reward = reward_function(float(e), float(rec_item_idx))
+    ## reward_function ini buat prediksi aja usernya bakal suka
+    ## atau engga dari item yang direkomendasiin
+    ## reward_function juga mirip-mirip model prediksi
+    
+    if reward > 0:
+        history_buffer.push(tf.identity(tf.stop_gradient(rec_item_emb)))
+        next_state = state_rep_net(user_emb, tf.stack(history_buffer.to_list()))
+    else:
+        next_state = tf.stop_gradient(state)
+    
+    user_ignored_items.append(rec_item_idx)
+    t+=1
+    
+for item in recommend_item:
+    print(items.get(item))
